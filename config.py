07a1b24016977a7cb8005e6acabe9da7fd041ce9
AlexandraKuznetsova1 <73255899+AlexandraKuznetsova1@users.noreply.github.com>
2025-01-29 19:43:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = '7872738868:AAFG6skg9WXfyZmLpgg0nCgu1QkuWkE-QiM'
+TELEGRAM_TOKEN = '7872738868:AAFG6skg9WXfyZmLpgg0nCgu1QkuWkE-Qi'
 API_TOKEN = 'e4oEaZY1Kom5OXzybETkMlwjOCy3i8GSCGTHzWrhd4dc563b'
 IMEI_CHECK_URL = 'https://imeicheck.net/api/check'  # URL для проверки IMEI
 
